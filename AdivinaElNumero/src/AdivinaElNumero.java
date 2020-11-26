574b2187b50604588f7a70bc4b6f744a40248aee
Jordy <39038507+SystemSickness@users.noreply.github.com>
2020-11-25 18:12:31 -0600

    Add files via upload

---
+++ AdivinaElNumero/src/AdivinaElNumero.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class AdivinaElNumero {
+    private Scanner entrada = new Scanner(System.in);
+    private boolean juegoActivo = true;
+    public void jugar (){
+        String nombreJugador = obtenerNumbreJugador();
+        while (juegoActivo) {
+            int intentos = 0;
+            int min = 0;
+            int max = 100;
+            int numeroJuego = obtenerNumeroAleatorio(1,100);
+
+            System.out.println(numeroJuego);
+            System.out.printf("%s, he escogido un numero entre %d y %d, adivinalo\n", nombreJugador, min, max);
+            int numeroJugador;
+
+            do {
+                numeroJugador = escogerNumero();
+                mensaje(numeroJuego, numeroJugador);
+                intentos++;
+            } while(numeroJuego != numeroJugador);
+
+            System.out.printf("Felicidades!!! has adivinado, con %d intentos ", intentos);
+
+
+            juegoActivo = jugarNuevamente();
+        }
+    }
+
+    private int obtenerNumeroAleatorio(int min, int max){
+
+        return min + (int) (Math.random() * ((max - min) + 1));
+    }
+
+    private String obtenerNumbreJugador() {
+        System.out.println("Hola, ¿Cual es tu nombre?");
+        String nombreJugador = entrada.nextLine();
+        System.out.printf("Bienvenido %s!!! Vamos a Comenzar\n", nombreJugador);
+        return nombreJugador;
+    }
+
+    private int escogerNumero () {
+        System.out.println("Escoge un número");
+        return entrada.nextInt();
+    }
+
+    private void mensaje (int numeroJuego, int numeroJugador){
+        if (numeroJugador > numeroJuego) {
+            System.out.println("Muy alto, adivina otra vez");
+        } else if (numeroJuego > numeroJugador) {
+            System.out.println("Muy bajo, adivina otra vez");
+        }
+    }
+
+    public boolean jugarNuevamente (){
+        System.out.println("¿Jugar nuevamente? \n (1) SI\n (2) NO");
+        int respuesta = entrada.nextInt();
+        if (respuesta == 1){
+            System.out.println("Estupendo!!");
+            return true;
+        }else {
+            System.out.println("GAME OVER!!");
+            return false;
+        }
+    }
+}
